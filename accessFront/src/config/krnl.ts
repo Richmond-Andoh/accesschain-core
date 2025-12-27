8cec2eb07cd3e9bbb9986515b9fe9f5bbf6a9ab4
richmond-andoh <richmondandoh00@gmail.com>
2025-12-27 08:57:20 +0000

    Refactoring the file structure

--- accessFront/src/config/krnl.ts
+++
@@ -1,6 +0,0 @@
-import { KRNLConfig } from '@krnl-dev/sdk-react-7702';
-
-export const krnlConfig: KRNLConfig = {
-    projectId: import.meta.env.VITE_KRNL_PROJECT_ID || '',
-    network: 'sonic-blaze'
-};
